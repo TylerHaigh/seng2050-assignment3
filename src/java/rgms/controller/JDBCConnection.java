3ba788196db549f4bb76fab7adb65abecd3a6e2d
Tyler Haigh <tylerhaigh1994@me.com>
2014-09-30 19:51:34 +1000

    Adds the database connection objects

---
+++ src/java/rgms/controller/JDBCConnection.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rgms.controller;
+
+import	java.sql.*;
+
+/**
+ *
+ * @author Tyler 2
+ */
+public class JDBCConnection {
+
+    //My SQL Connection String
+    private String databaseHost = "localhost";
+    private String databaseName = "databaseName";
+    private String dbUrl = "jdbc:mysql://" + databaseHost + "/" + databaseName;
+    
+    //My SQL Credentials
+    private String userName = "root";
+    private String password = "password";
+    
+    private Connection conn; //Create connection variable
+    
+    /**
+     * Constructor for a JDBC connection
+     * 
+     * @throws ClassNotFoundException The driver class cannot be found
+     * @throws InstantiationException Cannot instantiate a connection to the
+     *                                JDBC driver
+     * @throws IllegalAccessException Unable to access the JDBC driver
+     */		
+    public JDBCConnection() throws ClassNotFoundException,
+            InstantiationException, IllegalAccessException {
+        //Load the database driver
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+    }
+    
+    /**
+     * Creates a connection to the JDBC Database
+     * @throws SQLException The connection cannot be established
+     */
+    private void getConnection() throws	SQLException {
+        //Check if the connection does not exist
+        if(conn	== null || conn.isClosed()) {
+            //Establish a new connection
+            conn = DriverManager.getConnection(dbUrl,userName,password);
+        }
+    }
+    
+    /**
+     * Closes the connection to the JDBC Database
+     * @throws SQLException The connection cannot be closed
+     */
+    private void setDone() throws SQLException {
+        //Check the connection still exists
+        if(conn!=null) {
+            //Close the connection
+            conn.close();
+            conn = null;
+        }
+    }
+
+    /**
+     * 
+     * @return
+     * @throws SQLException 
+     */
+    public ResultSet executeQuery(String query) throws SQLException {
+        //Establish the connection
+        getConnection();
+        
+        ResultSet rs = null;
+        
+        //Exeute the query
+        try {
+            Statement s = conn.createStatement();
+            rs = s.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        //Close the connection and return the results
+        setDone();
+        return rs;
+    }
+    
+    
+}
