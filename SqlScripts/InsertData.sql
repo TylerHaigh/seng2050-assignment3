03ca7c9463c2223e52fe381f75bdb9c04a5800f0
Tyler Haigh <tylerhaigh1994@me.com>
2014-10-08 19:24:18 +1100

    Replaces Netbeans project with Eclipse Project
    Fingers crossed everything ported across ok

--- SqlScripts/InsertData.sql
+++ SqlScripts/InsertData.sql
@@ -1,5 +1,5 @@
-INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase) VALUES('Tyler', 'Haigh', 'c3182929', 'tyler');
-INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase) VALUES('Simon', 'Hartcher', 'c3182930', 'simon');
-INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase) VALUES('Joshua', 'Crompton', 'c3182940', 'josh');
-INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase) VALUES('Beau', 'Gibson', 'c3182950', 'beau');
-INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase) VALUES('Robert', 'Logan', 'c3182960', 'rob');
+INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase, ImageReference) VALUES('Tyler', 'Haigh', 'c3182929', 'tyler', 'c3182929-image.jpg');
+INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase, ImageReference) VALUES('Simon', 'Hartcher', 'c3182930', 'simon', 'c3182930-image.jpg');
+INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase, ImageReference) VALUES('Joshua', 'Crompton', 'c3182940', 'josh', 'c3182940-image.jpg');
+INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase, ImageReference) VALUES('Beau', 'Gibson', 'c3182950', 'beau', 'c3182950-image.jpg');
+INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase, ImageReference) VALUES('Robert', 'Logan', 'c3182960', 'rob', 'c3182960-image.jpg');
---
+++ WebContent/pages/EditProfile.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : EditProfile
+    Created on : 05/10/2014, 4:10:48 PM
+    Author     : Tyler 2
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
