0fd2797acdac90991f04eb25e8d0c5f7545b8a25
Tyler Haigh <tylerhaigh1994@me.com>
2014-10-12 21:09:51 +1100

    Adds inserts for new Schema

--- SqlScripts/InsertData.sql
+++ SqlScripts/InsertData.sql
@@ -1,5 +1,9 @@
-INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase, ImageReference) VALUES('Tyler', 'Haigh', 'c3182929', 'tyler', '../Uploads/images/c3182929-image.jpg');
-INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase, ImageReference) VALUES('Simon', 'Hartcher', 'c3182930', 'simon', '../Uploads/images/c3182930-image.jpg');
-INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase, ImageReference) VALUES('Joshua', 'Crompton', 'c3182940', 'josh', '../Uploads/images/c3182940-image.jpg');
-INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase, ImageReference) VALUES('Beau', 'Gibson', 'c3146845', 'beau', '../Uploads/images/c3146845-image.jpg');
-INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase, ImageReference) VALUES('Robert', 'Logan', 'c3182960', 'rob', '../Uploads/images/c3182960-image.jpg');
+INSERT INTO RGMS_DB.Groups (GroupName) VALUES ('Aggregates');
+INSERT INTO RGMS_DB.Groups (GroupName) VALUES ('PcMasterRace');
+INSERT INTO RGMS_DB.Groups (GroupName) VALUES ('ConsoleWars');
+
+INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase, ImageReference, GroupId, IsAdmin) VALUES('Tyler', 'Haigh', 'c3182929', 'tyler', '../Uploads/images/c3182929-image.jpg', 1, true);
+INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase, ImageReference, GroupId, IsAdmin) VALUES('Simon', 'Hartcher', 'c3182930', 'simon', '../Uploads/images/c3182930-image.jpg', 1, true);
+INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase, ImageReference, GroupId, IsAdmin) VALUES('Joshua', 'Crompton', 'c3182940', 'josh', '../Uploads/images/c3182940-image.jpg', 1, true);
+INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase, ImageReference, GroupId, IsAdmin) VALUES('Beau', 'Gibson', 'c3146845', 'beau', '../Uploads/images/c3146845-image.jpg', 3, false);
+INSERT INTO RGMS_DB.Users (FirstName, LastName, Username, Passphrase, ImageReference, GroupId, IsAdmin) VALUES('Robert', 'Logan', 'c3182960', 'rob', '../Uploads/images/c3182960-image.jpg', 2, false);
