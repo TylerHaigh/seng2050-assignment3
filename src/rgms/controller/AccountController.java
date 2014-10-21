dc65ca854ced8bc1e9d7acd305421fd9feb267e1
Simon Hartcher <simon@simonhartcher.com>
2014-10-21 17:50:00 +1100

    Created abstract rgms.mvc.Controller class.

--- src/rgms/controller/AccountController.java
+++ src/rgms/controller/AccountController.java
@@ -9,73 +9,18 @@ import java.util.logging.*;
 import java.util.regex.*;
 import java.lang.reflect.Method;
 import java.lang.reflect.InvocationTargetException;
+import rgms.mvc.*;
 
 @WebServlet(urlPatterns = { "/account/*", "/account" })
-public class AccountController extends HttpServlet {
+public class AccountController extends Controller {
   private static final Logger logger = Logger.getLogger(AccountController.class.getName());
 
   public AccountController() { }
 
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-    routeRequest(req, resp);
-  }
-
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
-    routeRequest(req, resp);
-  }
-
-  private void loginAction(HttpServletRequest req, HttpServletResponse res) {
+  public void loginAction(HttpServletRequest req, HttpServletResponse res) {
     String viewFile = "/views/account/Login.jsp";
 
     view(req, res, viewFile);
   }
 
-  private Method getActionMethod(String actionName) {
-    Method[] methods = AccountController.class.getDeclaredMethods();
-    for (Method m: methods) {
-      if (m.getName().equals(actionName + "Action")) {
-        return m;
-      }
-    }
-
-    return null;
-  }
-
-  public void view(HttpServletRequest req, HttpServletResponse res, String viewPath) {
-    try {
-      RequestDispatcher view = req.getRequestDispatcher(viewPath);
-      view.forward(req, res); 
-    }
-    catch (ServletException e) {
-      logger.log(Level.SEVERE, "Servlet Error", e);
-    }
-    catch (IOException e) {
-      logger.log(Level.SEVERE, "IO Error", e);
-    }
-  }
-
-  public void routeRequest(HttpServletRequest req, HttpServletResponse res) {
-    try {
-      //get action name
-      String actionName = Controller.getAction(req.getRequestURL().toString());
-
-      //find action method on class
-      Method actionMethod = this.getActionMethod(actionName);
-
-      //no action found
-      if (actionMethod == null) {
-        //404 then return
-        res.sendError(HttpServletResponse.SC_NOT_FOUND);
-        return;
-      }
-
-      actionMethod.invoke(this, req, res);
-    }
-    catch (IOException e) {
-      logger.log(Level.SEVERE, "IO Error", e);
-    }
-    catch (IllegalAccessException | InvocationTargetException e) {
-      logger.log(Level.SEVERE, "Action Method Error", e);
-    }
-  }
 }
---
+++ src/rgms/mvc/Controller.java
@@ -0,0 +1,90 @@
+package rgms.mvc;
+
+import java.io.*;
+import java.net.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.util.logging.*;
+import java.util.regex.*;
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+
+public abstract class Controller extends HttpServlet {
+  private static final Logger logger = Logger.getLogger(Controller.class.getName());
+
+  private static String getAction(URI requestUri) {
+    String[] paths = requestUri.getPath().toLowerCase().split("/");
+    String actionName = paths[paths.length - 1];
+    System.out.println(actionName);
+
+    return actionName;
+  }
+
+  private static String getAction(String requestString) {
+    try {
+      return getAction(new URI(requestString));
+    }
+    catch (URISyntaxException e) {
+      logger.log(Level.SEVERE, "URI Error", e);
+      return null;
+    }
+  }
+
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+    routeRequest(req, resp);
+  }
+
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    routeRequest(req, resp);
+  }
+
+  private Method getActionMethod(String actionName) {
+    Method[] methods = this.getClass().getDeclaredMethods();
+    for (Method m: methods) {
+      if (m.getName().equals(actionName + "Action")) {
+        return m;
+      }
+    }
+
+    return null;
+  }
+
+  protected void view(HttpServletRequest req, HttpServletResponse res, String viewPath) {
+    try {
+      RequestDispatcher view = req.getRequestDispatcher(viewPath);
+      view.forward(req, res); 
+    }
+    catch (ServletException e) {
+      logger.log(Level.SEVERE, "Servlet Error", e);
+    }
+    catch (IOException e) {
+      logger.log(Level.SEVERE, "IO Error", e);
+    }
+  }
+
+  private void routeRequest(HttpServletRequest req, HttpServletResponse res) {
+    try {
+      //get action name
+      String actionName = Controller.getAction(req.getRequestURL().toString());
+
+      //find action method on class
+      Method actionMethod = this.getActionMethod(actionName);
+
+      //no action found
+      if (actionMethod == null) {
+        //404 then return
+        res.sendError(HttpServletResponse.SC_NOT_FOUND);
+        return;
+      }
+
+      actionMethod.invoke(this, req, res);
+    }
+    catch (IOException e) {
+      logger.log(Level.SEVERE, "IO Error", e);
+    }
+    catch (IllegalAccessException | InvocationTargetException e) {
+      logger.log(Level.SEVERE, "Action Method Error", e);
+    }
+  }
+}
