324fc12f42c44dcc06fc8fa55f89f912ff2a6f10
Tyler Haigh <tylerhaigh1994@me.com>
2014-11-02 15:19:37 +1100

    Adds Notification Schema and Model

---
+++ src/rgms/model/Notification.java
@@ -0,0 +1,128 @@
+package rgms.model;
+
+import java.io.Serializable;
+
+public class Notification implements Serializable {
+
+	//Private instance variables
+	private int id;
+	
+	private int userId;
+	private User user;
+	
+	private int groupId;
+	private Group group;
+	
+	private int meetingId;
+	private Meeting meeting;
+	
+	private int discussionPostId;
+	private DiscussionPost discssionPost;
+	
+	//Constructors
+	
+	public Notification() {
+		
+		this.id = 1;
+		this.userId = 1;
+		this.user = new User();
+		this.groupId = 1;
+		this.group = new Group();
+		this.meetingId = 1;
+		this.meeting = new Meeting();
+		this.discussionPostId = 1;
+		this.discssionPost = new DiscussionPost();
+	}
+	
+	public Notification(int id, int userId, User user, int groupId,
+			Group group, int meetingId, Meeting meeting, int discussionPostId,
+			DiscussionPost discssionPost) {
+		
+		this.id = id;
+		this.userId = userId;
+		this.user = user;
+		this.groupId = groupId;
+		this.group = group;
+		this.meetingId = meetingId;
+		this.meeting = meeting;
+		this.discussionPostId = discussionPostId;
+		this.discssionPost = discssionPost;
+	}
+
+	//Getters
+	
+	public int getId() {
+		return id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public int getGroupId() {
+		return groupId;
+	}
+
+	public Group getGroup() {
+		return group;
+	}
+
+	public int getMeetingId() {
+		return meetingId;
+	}
+
+	public Meeting getMeeting() {
+		return meeting;
+	}
+
+	public int getDiscussionPostId() {
+		return discussionPostId;
+	}
+
+	public DiscussionPost getDiscssionPost() {
+		return discssionPost;
+	}
+
+	//Setters
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public void setGroupId(int groupId) {
+		this.groupId = groupId;
+	}
+
+	public void setGroup(Group group) {
+		this.group = group;
+	}
+
+	public void setMeetingId(int meetingId) {
+		this.meetingId = meetingId;
+	}
+
+	public void setMeeting(Meeting meeting) {
+		this.meeting = meeting;
+	}
+
+	public void setDiscussionPostId(int discussionPostId) {
+		this.discussionPostId = discussionPostId;
+	}
+
+	public void setDiscssionPost(DiscussionPost discssionPost) {
+		this.discssionPost = discssionPost;
+	}
+
+}
