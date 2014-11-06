300bc8f7b253d3af7877f7a30deddeac9b4f1b99
Tyler Haigh <tylerhaigh1994@me.com>
2014-11-06 15:04:18 +1100

    Performs validation on Meeting Date

--- WebContent/js/formvalidation.js
+++ WebContent/js/formvalidation.js
@@ -119,6 +119,46 @@ function validateMeeting(code) {
 		valid = false;
 	}
 	
+	//Validate Meeting Date
+	var validDate = true;
+	var endMonth = x.indexOf("/");
+	var endDay = x.indexOf("/", endMonth + 1);
+	
+	if (endMonth == -1 || endDay == -1) {
+		validDate = false;
+	} else {
+		var month = parseInt(x.substring(0, endMonth));
+		var day = parseInt(x.substring(endMonth + 1, endDay));
+		var year = parseInt(x.substring(endDay + 1, x.length));
+		
+		if (isNaN(month) || isNaN(day) || isNaN(year)) {
+			validDate = false;
+		} else if (month < 0 || day < 0 || year < 0) {
+			validDate = false
+		} else if (month > 12 || year > 999999) {
+			validDate = false
+		} else {
+			if (month == 1 || month == 3 || month == 5 || month == 7 || month == 10 || month == 12) {
+				if (day > 31) {
+					validDate = false;
+				}
+			} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+				if (day > 30) {
+					validDate = false;
+				}
+			} else if (month == 2 && (year % 4) == 0 && day > 29) {
+				validDate = false;
+			} else if (month == 2 && day > 28) {
+				validDate = false;
+			}
+		}
+	}
+	
+	if (validDate == false) {
+		msg += "Meeting Date format is invalid. Expected format: MM/dd/yyyy. \n";
+		valid = false;
+	}
+	
 	x = document.getElementById("meetingTime").value;
 	if (x =="" || x == null) {
 		msg += "Meeting Time was left blank. \n";
@@ -145,7 +185,7 @@ function validateMeeting(code) {
 	}
 	
 	if (validTime == false) {
-		msg += "Meeting time format is invalid. Expected format: HH:mm. \n";
+		msg += "Meeting Time format is invalid. Expected format: HH:mm. \n";
 		valid = false;
 	}
 		
